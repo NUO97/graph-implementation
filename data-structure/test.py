467930a22476fef3e934f69459d8a75ffb3008ca
Nuo Chen <nuo@D-10-18-225-222.dhcp4.washington.edu>
2019-02-13 13:10:21 -0800

    Fixed issue in traversal and edge creation

---
+++ data-structure/test.py
@@ -0,0 +1,14 @@
+from graph import AGraph
+
+# G = AGraph()
+#
+# G.create_node("Math")
+#
+# print(G.nodes[0].data)
+#
+# print(G.size)
+
+#
+# G = AGraph.generate_random(10, 0.3)
+#
+# print(G.size)
